5428a1bbe1fbcb684ca9e7c9779a3a0de3a99937
wangying <429009190@qq.com>
2020-09-11 18:48:13 +0800

    changing

--- src/common/Header.vue
+++ src/common/Header.vue
@@ -4,7 +4,7 @@
       <Button class="btn"
               icon="back"
               @click="goBack"></Button>
-      <div class="title">{{title}}哈哈哈</div>
+      <div class="title">{{title}}哈嗷嗷</div>
     </div>
   </div>
 </template>
